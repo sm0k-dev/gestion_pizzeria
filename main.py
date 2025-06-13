e2909caae21e8e55c9cf343bf2add163d529f615
sm0k-dev <be.albornozc@duocuc.cl>
2025-06-13 11:32:25 -0400

    Hotfix: Facilitar lectura del código (comentarios)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Libraries
 import os, time, msvcrt
 from functions import *
+
 #MenuText
 menu = """\n=== GESTIÓN DE PEDIDOS ===
 1) Registrar nuevas pizzas disponibles para la venta.
@@ -9,6 +10,7 @@ menu = """\n=== GESTIÓN DE PEDIDOS ===
 4) Ver pedidos realizados.
 0) Salir del sistema."""
 #MenuText
+
 #Menu
 while True:
     #Welcome - CleanScreen
@@ -17,30 +19,37 @@ while True:
     option = input("Ingrese opción (0-3): ")
     os.system('cls')
     #Welcome - CleanScreen
+
     #Options
     #Option_1: Registrar pizzas.
     if option == '1':
         registrar_pizza()
     #Option_1
+        
     #Option_2: Ver catálogo pizzas.
     elif option == '2':
         pass
     #Option_2
+
     #Option_3: Realizar pedido.
     elif option == '3':
         pass
     #Option_3
+
     #Option_4: Ver pedidos.
     elif option == '4':
         pass
     #Option_4
+
     #Option_0: Salir del sistema.
     elif option == '0':
         print("...Saliendo del sistema...")
         time.sleep(1)
     #Option_0
+    #OptionError
     else:
         print("Error. Opción inválida.")
+
     #OptionEnd - KeyToContinue
     print("...Presione una tecla para volver al menú principal...")
     msvcrt.getch()
